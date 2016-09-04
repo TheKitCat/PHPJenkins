0b1f6c75820ef61464850b9d6d8df7e94ce6de33
KitCat <alyyn84@gmail.com>
2016-09-04 09:54:14 +0200

    snd

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php
+class TestPHP{
+    
+    /**
+     * @assert (2, 1) == 3
+     * @assert (1, 1) == 2
+     * @assert (5, 5) == 10
+     * @param type $a
+     * @param type $b
+     * @return type
+     */
    function add($a,$b){
        
         return $a+$b;  
        
    }
-   echo add(3,5);
+   
+}
+
+$obj = new TestPHP;
+echo $obj->add(1, 2);
 ?>
